bfe6531b67162283c0a98b156717f06ed7507aea
Julian Togelius <julian@togelius.com>
2017-11-23 15:06:51 +0100

    Test merge

--- src/json/Test.java
+++ src/json/Test.java
@@ -1,13 +1,12 @@
 package json;
-
+import com.google.Gson;
 /**
  * Created by julian on 11/23/17.
  */
 public class Test {
 
     public static void main(String[] args) {
-        JSONParser parser = new JSONParser ("../MatchTilesAtDagstuhl/examples/Bejeweled.mtg");
-        System.out.println("created parser");
+        Gson gson = new Gson();
 
     }
 
