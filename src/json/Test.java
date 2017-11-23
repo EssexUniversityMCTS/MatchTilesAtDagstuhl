30416fd1ba62f4306edcfd7dfe4cbac6f0777600
Julian Togelius <julian@togelius.com>
2017-11-23 16:19:06 +0100

    mtg file fix

--- src/json/Test.java
+++ src/json/Test.java
@@ -2,16 +2,22 @@ package json;
 import core.Game;
 import tools.com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  * Created by julian on 11/23/17.
  */
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String filename = "../MatchTilesAtDagstuhl/examples/Bejeweled.mtg";
+        System.out.println("Trying to read " +  filename);
+        BufferedReader br = new BufferedReader (new FileReader(filename));
         String json = null;
         Gson gson = new Gson();
         Game game = gson.fromJson (json, Game.class);
-
+        System.out.println(game);
     }
 
 }
