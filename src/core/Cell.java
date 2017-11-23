f810a520522f5090cab7aded177e88850cfeba02
diegopliebana <email@example.com>
2017-11-23 15:19:06 +0100

    cells

---
+++ src/core/Cell.java
@@ -0,0 +1,15 @@
+package core;
+
+/**
+ * Created by dperez on 23/11/2017.
+ */
+public class Cell
+{
+    public int[] colour;
+
+    public Cell(int[] colour)
+    {
+        this.colour = colour;
+    }
+
+}
--- src/core/Grid.java
+++ src/core/Grid.java
@@ -1,5 +1,7 @@
 package core;
 
+import java.util.Random;
+
 /**
  * Created by julian on 11/23/17.
  */
@@ -11,4 +13,33 @@ public class Grid {
     String blocks;
     String color;
 
+    Cell[][] grid;
+
+    public Grid() {
+        //init();
+    }
+
+    public void init() {
+//        if(type.equalsIgnoreCase("Square"))
+//        {
+//
+//        }
+
+        Random rnd = new Random();
+
+        grid = new Cell[size][size];
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                grid[i][j] = new Cell(new int[]{rnd.nextInt(2), rnd.nextInt(2), rnd.nextInt(2)});
+            }
+        }
+
+    }
+
+    public static void main(String args[]) {
+        Grid g = new Grid();
+        g.size = 5;
+        g.init();
+        int a = 0;
+    }
 }
